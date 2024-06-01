f332663c6ab65e2c1603837741db1d38c45231ac
johnh <jhuan47@wgu.edu>
2024-06-01 16:11:58 -0400

    loads socketio into app.py, creates .env file to store secret key, updates title of index.html

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
