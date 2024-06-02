6df63f16848e6007f25147ace2243ae281eda950
johnh <jhuan47@wgu.edu>
2024-06-02 11:25:03 -0400

    adds socket decorator to receive message and print to terminal

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ socketio = SocketIO(app)
 def hello_world():
     return render_template('index.html')
 
+@socketio.on('message')
+def handle_message(data):
+    print('received message: ' + data)
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
